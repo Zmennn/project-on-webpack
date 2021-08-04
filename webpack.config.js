a85e8550f5f6e66ac81867c8b1b889356681e000
Zmennn <79196210+Zmennn@users.noreply.github.com>
2021-08-04 23:17:57 +0300

    0.3

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,60 @@
 // стандартное обращение к файловой системе в среде ноды
 const path = require('path');
 
+//плагин очистки
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+//плагин для работы с html
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+
+
+
 //Объект настроек вебпака
 module.exports = {
 
     //точка входа
     entry: {
-        main: path.resolve()
+        main: path.resolve(__dirname, './src/index.js')
+    },
+
+    //точка выхода
+    output: {
+        //задаст путь и создаст папку для выходных материалов
+        path: path.resolve(__dirname, './dist'),
+
+        //зададим имена создаваемым файлам
+        filename: '[name].bundle.js'
+    },
+
+
+    plugins: [
+
+        //конструктор плагина очистки
+        new CleanWebpackPlugin(),
+
+        //конструктор плагина html
+        new HtmlWebpackPlugin({
+            //настройка заголовка в шаблоне
+            title: 'title in webpack settings',
+            //задать путь к шаблону
+            template: path.resolve(__dirname, './src/template.html'),
+            //зададим имя создаваемому файлу
+            filename: 'index.html',
+            //зададим куда вставлять скрипты js
+            inject: 'body',
+
+        }),
+    ],
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            }
+        ]
     }
+
 }
